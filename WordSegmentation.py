677e607b153d574774f72d3d8ec4b272df0ceadb
WenqingCao <caowenqi@usc.edu>
2017-03-23 00:32:53 -0700

    Modified word segmentation
    “zh.json” is stop word list. “sougou.dic” is the dictionary to help
    improve word segmentation accuracy.

--- WordSegmentation.py
+++ WordSegmentation.py
@@ -1,8 +1,22 @@
 import jieba
-file = open("dev_data.txt")
+import json
+
+#Read stop word file
+with open("zh.json") as stopwordfile:
+    stopWord = json.load(stopwordfile)
+    stopwordfile.close()
+
+#Load word dictionary to improve word segmentation accuracy
+jieba.load_userdict("sougou.dic")
+
 #File's total line number is 20000, set 75% (15000 lines) as development data
+file = open("dev_data.txt")
+#user's information matrix
 userInfo = {}
+#user's query list
 queryList = {}
+
+#read file and segment word
 for line in file:
     line = line.strip()
     item = line.split("\t")
@@ -16,6 +30,10 @@ for line in file:
             queryList[usrID] = []
         query = item[i]
         wordlist = jieba.cut_for_search(query)
-        res = ",".join(wordlist)
-        #print res
+        res = ""
+        for word in wordlist:
+            if word in stopWord:
+                continue
+            res += " " + word
+        print res
         queryList[usrID].append(res)
